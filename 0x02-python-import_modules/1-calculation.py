2dca553fddbe8e754ac51b0b31bb9771a72f1c18
abiola badaru <abdulbasitalism@gmail.com>
2024-01-04 17:14:23 -0800

    my task one complete

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# a program that imports functions from the file calculator_1.py, does some Maths, and prints the result
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
