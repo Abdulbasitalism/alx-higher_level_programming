7625d32209078f97df7b0494b5110efa5fa4bb91
abiola badaru <abdulbasitalism@gmail.com>
2024-01-04 17:12:32 -0800

    my task one completed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import addition, subtraction, product, quotient
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, addition(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, subtraction(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, product(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, quotient(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
