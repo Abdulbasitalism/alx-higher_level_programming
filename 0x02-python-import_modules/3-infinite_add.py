8fc25dcec33b2da1203d8da73b260d5f16fc23e7
abiola badaru <abdulbasitalism@gmail.com>
2024-01-06 13:54:59 -0800

    my task three done well

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# A program that prints the result of the addition of all arguments
+# a program that prints the result of the addition of all arguments
 if __name__ == "__main__":
-    from sys import argv
-    sumint = 0
-    for i in range(1, len(argv)):
-        sumint += int(argv[i])
-        print("{}".format(sumint))
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
