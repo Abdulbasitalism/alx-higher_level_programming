a098152c7fc86823d10cca778c77c9df7ff38a64
abiola badaru <abdulbasitalism@gmail.com>
2024-01-06 23:35:36 -0800

    my task five on no C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a function that removes all characters c from string
+def no_c(my_string):
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
