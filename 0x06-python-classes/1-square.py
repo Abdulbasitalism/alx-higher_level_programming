4a081f9c1e93da70471be5c8c7b98649c884913f
abiola badaru <abdulbasitalism@gmail.com>
2024-01-23 13:57:40 -0800

    my tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a classwhich is a class Square."""
+
+
+class Square:
+    """This ckass represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
